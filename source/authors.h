ed58096f072deac7180b14c0c77d0877054aef0b
Duc_Thai_Tran <thaitranduc2001@gmail.com>
2024-02-02 22:10:22 +0100

    test commit, with authors.h updated

--- source/authors.h
+++ source/authors.h
@@ -1,16 +1,16 @@
 #pragma once
 
-#define AUTHORS_ONE_A "Duc Thai Tran, Esra Akyüz"
-#define AUTHORS_ONE_B "Duc Thai Tran, Esra Akyüz"
-#define AUTHORS_ONE_C "Duc Thai Tran, Esra Akyüz"
-#define AUTHORS_ONE_D "Duc Thai Tran, Esra Akyüz"
+#define AUTHORS_ONE_A "Duc Thai Tran, Esra Akyuez, Diep Anh Le Pham"
+#define AUTHORS_ONE_B "Duc Thai Tran, Esra Akyuez, Diep Anh Le Pham"
+#define AUTHORS_ONE_C "Duc Thai Tran, Esra Akyuez, Diep Anh Le Pham"
+#define AUTHORS_ONE_D "Duc Thai Tran, Esra Akyuez, Diep Anh Le Pham"
 
-#define AUTHORS_TWO_A "Duc Thai Tran, Esra Akyüz"
-#define AUTHORS_TWO_B "Duc Thai Tran, Esra Akyüz"
-#define AUTHORS_TWO_C "Duc Thai Tran, Esra Akyüz"
+#define AUTHORS_TWO_A "Duc Thai Tran, Esra Akyuez, Diep Anh Le Pham"
+#define AUTHORS_TWO_B "Duc Thai Tran, Esra Akyuez, Diep Anh Le Pham"
+#define AUTHORS_TWO_C "Duc Thai Tran, Esra Akyuez, Diep Anh Le Pham"
 
-#define AUTHORS_THREE_A "Duc Thai Tran, Esra Akyüz"
-#define AUTHORS_THREE_B "Duc Thai Tran, Esra Akyüz"
-#define AUTHORS_THREE_C "Duc Thai Tran, Esra Akyüz"
-#define AUTHORS_THREE_D "Duc Thai Tran, Esra Akyüz"
-#define AUTHORS_THREE_E "Duc Thai Tran, Esra Akyüz"
+#define AUTHORS_THREE_A "Duc Thai Tran, Esra Akyuez, Diep Anh Le Pham"
+#define AUTHORS_THREE_B "Duc Thai Tran, Esra Akyuez, Diep Anh Le Pham"
+#define AUTHORS_THREE_C "Duc Thai Tran, Esra Akyuez, Diep Anh Le Pham"
+#define AUTHORS_THREE_D "Duc Thai Tran, Esra Akyuez, Diep Anh Le Pham"
+#define AUTHORS_THREE_E "Duc Thai Tran, Esra Akyuez, Diep Anh Le Pham"
