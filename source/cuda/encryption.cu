dbe85e2123297766f644d3e96363e79ea1238aef
DiepAnhLP <92036220+DiepAnhLP@users.noreply.github.com>
2024-02-05 10:20:35 +0100

    Update encryption.cu
    Commit 3a

--- source/cuda/encryption.cu
+++ source/cuda/encryption.cu
@@ -1,6 +1,19 @@
 #include "encryption.cuh"
+#include "util/Hash.h"
 
 #include <cooperative_groups.h>
 #include <cuda.h>
 #include <cuda_runtime_api.h>
 #include <device_launch_parameters.h>
+
+/** Kernel of hash function **/
+__global__ void hash(const std::uint64_t* const values, std::uint64_t* const hashes, const unsigned int length){
+    // Calculate the global index of the current thread
+    unsigned int index = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // Check if thread is within length
+    if(index < length){
+        std::uint64_t hash_value = Hash::hash(values[index]);
+        hashes[index] = hash_value;
+    }
+}
