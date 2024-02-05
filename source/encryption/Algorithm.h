4b952c28d09931c6101c07e1f52b3b8e149c05b0
DiepAnhLP <92036220+DiepAnhLP@users.noreply.github.com>
2024-02-05 10:28:01 +0100

    Update Algorithm.h
    Commit 3f

--- source/encryption/Algorithm.h
+++ source/encryption/Algorithm.h
@@ -25,4 +25,42 @@ public:
 	// task 1 d)
 	[[nodiscard]] BitmapImage perform_scheme(BitmapImage bmi, Key::key_type keytype, EncryptionScheme e_scheme);
 
+	// task 3 e)
+	/** retrieve_scheme function **/
+	EncryptionScheme retrieve_scheme(std::uint64_t c){
+    		EncryptionScheme result;
+
+   		 // Iterate over all possible scheme
+    		// 1ULL is the integer literal 1 (ULL stands for "Unsigned Long Long"), represented as an unsigned 64-bit integer
+    		// (1ULL << 20) equals 2^20
+    		for(auto i = std::uint64_t(0); i < (1ULL << 20); i++){
+        		std::uint64_t encoded_scheme = encode(i);
+
+		        // Check if the hash matches encoded scheme
+        		if(c == encoded_scheme){
+            			result = decode(i);
+
+            			// Get standard key for first encryption
+            			Key::key_type encryption_key = Key::get_standard_key();
+ 	           		BitmapImage = image;
+
+       	   			int step = 0;
+      		      		// First 10 steps
+        			while (step < 10) {
+                			image = perform_scheme(result, encryption_key, encoded_scheme);
+                			step++;
+            			}
+
+		        	// Next 6 steps
+            			while (step < 16) {
+					encode(decoded_scheme[step]);
+					step++;
+				}
+
+		        	break;
+        		}
+    		}
+   		return result;
+	}
+
 };
